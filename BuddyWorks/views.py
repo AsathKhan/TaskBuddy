9a1231d61466e4c587a8bbd129599edd7c6d70c0
AsathKhan <asath2k2@gmail.com>
2024-10-28 19:59:55 +0530

    role based access decorator

--- BuddyWorks/views.py
+++ BuddyWorks/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib import messages
 from .forms import LoginForm, SignUpForm
 
 # Create your views here.
 def index_view(request):
     return render(request, 'BuddyWorks/index.html')
+
+def role_required(role):
+    def decorator(view_func):
+        #this user_pass... will fetch the user object and gonna return to lambda
+        @user_passes_test(lambda u: u.is_authenticated and u.role == role)
+        def _main_func(request, *args, **kwargs):
+            return view_func(request, *args,**kwargs)
+        return _main_func
+    return decorator
 
 def login_view(request):
     if request.method == "POST":
