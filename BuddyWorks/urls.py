36d113475cad02c257d3c965fa2dc532acac8977
AsathKhan <asath2k2@gmail.com>
2024-10-29 18:54:09 +0530

    logout btn and redirect

--- BuddyWorks/urls.py
+++ BuddyWorks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from BuddyWorks.views import index_view
 from .views import login_view, signup_view
 from .views import dashboard_view
@@ -6,6 +7,7 @@ from .views import dashboard_view
 urlpatterns=[
     path('', index_view, name="indexURL"),
     path('login/', login_view, name="loginURL"),
+    path('logout/', LogoutView.as_view(next_page='indexURL'), name='logoutURL'),
     path('signup/', signup_view, name="signupURL"),
     path('dashboard/', dashboard_view, name="dashboardURL"),
 ]
