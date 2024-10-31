896ff906d6156909bd1d0e96e60a0d8db4e4221c
AsathKhan <asath2k2@gmail.com>
2024-10-31 15:23:03 +0530

    admin support message upadated

--- BuddyWorks/admin.py
+++ BuddyWorks/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from .models import Profile, SupportMessage
 
 # Register your models here.
-admin.site.register(Profile)
-admin.site.register(SupportMessage)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ( 'user', 'role')
+admin.site.register(Profile, ProfileAdmin)
+
+class SupportMessageAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'created_at')
+admin.site.register(SupportMessage, SupportMessageAdmin)
