0ec62888118b2b3dc2e78e9f7f94337e9994d3b2
AsathKhan <asath2k2@gmail.com>
2024-10-31 11:21:58 +0530

    support js updated

--- BuddyWorks/static/BuddyWorks/js/support.js
+++ BuddyWorks/static/BuddyWorks/js/support.js
@@ -1,15 +1,24 @@
 const chatForm = document.getElementById('chat-box');
+const formMessages = document.getElementById("form-messages")
 
 chatForm.addEventListener('submit', (e) =>{
     e.preventDefault(); 
 
     const username = document.getElementById('username').value.trim();
+    const email = document.getElementById("email").value.trim();
     const message = document.getElementById('message').value.trim();
 
-    if (username == "" || message === ""){
-        alert("please fill out both your name and message.");
+    if (username == "" || email ==="" || message === ""){
+        // we used *required in input. so no use of this if block
+        displayMessage("please fill out all fields", "error");
     }else{
-        alert("Message sent! Our Support team will get back to you shortyly!");
+        displayMessage("Message sent! Our Support team will get back to you shortyly!", 'success');
         chatForm.reset();
     }
-})
+});
+
+function displayMessage(msg, type){
+    formMessages.textContent = msg;
+    formMessages.className = `message-box ${type}`;
+    formMessages.style.display= "block";
+}
