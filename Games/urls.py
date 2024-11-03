fe577a70497393fe3c30167b52d5a4a44ea8c414
AsathKhan <asath2k2@gmail.com>
2024-11-03 16:52:09 +0530

    games main pages

--- Games/urls.py
+++ Games/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from Games.views import dice_roller_view
+from .views import games_hub_view, dice_roller_view
 
 urlpatterns = [
     path('dice-roller/', dice_roller_view, name='dice_rollerURL'),
+    path('games-hub/', games_hub_view, name='games_hubURL' )
 ]
--- Games/views.py
+++ Games/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def games_hub_view(request):
+    return render(request, 'Games/games.html')
+
 def dice_roller_view(request):
     return render(request, 'Games/dice_roller.html')
